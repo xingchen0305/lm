0a3cf38ec6ce6530889835d25ac07a5d994b1d5c
CJ <1547974261@qq.com>
2017-09-20 16:39:20 +0800

    init

--- light_manage/src/main/java/com/bupt/controller/test.java
+++ light_manage/src/main/java/com/bupt/controller/test.java
@@ -1,1 +1,7 @@
 package com.bupt.controller;
+
+/**
+ * Created by CJ on 2017/9/20.
+ */
+public class test {
+}
--- light_manage/src/main/java/com/bupt/domain/test.java
+++ light_manage/src/main/java/com/bupt/domain/test.java
@@ -1,1 +1,7 @@
 package com.bupt.domain;
+
+/**
+ * Created by CJ on 2017/9/20.
+ */
+public class test {
+}
